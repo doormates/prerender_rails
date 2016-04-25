72407a97e25e1772f1922f46f05f25b1d0fabbe0
Jorge Davila <jorge@jdavila.me>
2016-04-25 16:36:10 -0400

    add bitly

--- lib/prerender_rails.rb
+++ lib/prerender_rails.rb
@@ -28,7 +28,8 @@ module Rack
         'vkShare',
         'W3C_Validator',
         'redditbot',
-        'Applebot'
+        'Applebot',
+        'bitlybot'
       ]
 
       @extensions_to_ignore = [
